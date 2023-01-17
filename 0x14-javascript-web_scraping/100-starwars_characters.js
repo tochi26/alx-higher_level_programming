18ef4fe3b2db5251f36deb76ddf09b661180aec4
Tochukwu <105463740+tochi26@users.noreply.github.com>
2023-01-17 22:46:32 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,47 +1,21 @@
 #!/usr/bin/node
-const request = require("request");
 
-async function getMovieCharacters(movieId) {
-  try {
-    // Make the request to the movie API endpoint
-    const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
-    const movieResponse = await requestPromise(movieUrl);
-    const movieData = JSON.parse(movieResponse);
-
-    // Get the character URLs from the movie data
-    const characterUrls = movieData.characters;
-
-    // Use map to make requests to each character's API endpoint and return the character name
-    const characterNames = await Promise.all(
-      characterUrls.map(async characterUrl => {
-        const characterResponse = await requestPromise(characterUrl);
-        const characterData = JSON.parse(characterResponse);
-        return characterData.name;
-      })
-    );
-
-    // Log each character name to the console
-    characterNames.forEach(name => console.log(name));
-  } catch (error) {
-    console.error(error);
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
   }
-}
-
-// Wrapper function for request to handle errors and return a promise
-function requestPromise(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
       if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(`Invalid status code: ${response.statusCode}`);
-      } else {
-        resolve(body);
+        console.log(error);
       }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
     });
-  });
-}
-
-// Get the movie ID from the command line argument
-const movieId = process.argv[2];
-getMovieCharacters(movieId);
+  }
+});
