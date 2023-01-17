f79df75cd8c88206f764183a6e7360a8136889cd
Tochukwu <105463740+tochi26@users.noreply.github.com>
2023-01-17 22:38:06 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+
+const movieId = process.argv[2]; // get the movie ID from the command line argument
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    characters.forEach(function(character) {
+      request(character, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
         }
       });
     });
