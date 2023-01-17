b3d980c4c2c31d1cd39d1154c6996f06749238e6
Tochukwu <105463740+tochi26@users.noreply.github.com>
2023-01-17 22:43:37 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,47 @@
 #!/usr/bin/node
-node script.js 3
+const request = require("request");
 
-const request = require('request');
+async function getMovieCharacters(movieId) {
+  try {
+    // Make the request to the movie API endpoint
+    const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
+    const movieResponse = await requestPromise(movieUrl);
+    const movieData = JSON.parse(movieResponse);
 
-const movieId = process.argv[2]; // get the movie ID from the command line argument
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+    // Get the character URLs from the movie data
+    const characterUrls = movieData.characters;
 
-request(apiUrl, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const characters = data.characters;
-    characters.forEach(function(character) {
-      request(character, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
-    });
+    // Use map to make requests to each character's API endpoint and return the character name
+    const characterNames = await Promise.all(
+      characterUrls.map(async characterUrl => {
+        const characterResponse = await requestPromise(characterUrl);
+        const characterData = JSON.parse(characterResponse);
+        return characterData.name;
+      })
+    );
+
+    // Log each character name to the console
+    characterNames.forEach(name => console.log(name));
+  } catch (error) {
+    console.error(error);
   }
-});
+}
+
+// Wrapper function for request to handle errors and return a promise
+function requestPromise(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(`Invalid status code: ${response.statusCode}`);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+// Get the movie ID from the command line argument
+const movieId = process.argv[2];
+getMovieCharacters(movieId);
